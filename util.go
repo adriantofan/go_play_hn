b197479880691364a603914db78e519e46f952e3
a <a>
2018-06-18 01:35:15 +0200

    added TimeRange

--- util.go
+++ util.go
@@ -20,3 +20,32 @@ func ParseTime(s string) time.Time {
 	t, _ := time.Parse(config.DateFormat, s)
 	return t.UTC()
 }
+
+// TimeRange given a formated datestring it tries to parse it and then returns an interval arround it
+func TimeRange(dateString string) (start time.Time, end time.Time, ok bool) {
+	t, err := time.Parse("2006-01-02 15:04:05", dateString)
+	if err == nil {
+		return t, t.Add(time.Second), true
+	}
+	t, err = time.Parse("2006-01-02 15:04", dateString)
+	if err == nil {
+		return t, t.Add(time.Minute), true
+	}
+	t, err = time.Parse("2006-01-02 15", dateString)
+	if err == nil {
+		return t, t.Add(time.Hour), true
+	}
+	t, err = time.Parse("2006-01-02", dateString)
+	if err == nil {
+		return t, t.AddDate(0, 0, 1), true
+	}
+	t, err = time.Parse("2006-01", dateString)
+	if err == nil {
+		return t, t.AddDate(0, 1, 0), true
+	}
+	t, err = time.Parse("2006", dateString)
+	if err == nil {
+		return t, t.AddDate(1, 0, 0), true
+	}
+	return time.Time{}, time.Time{}, false
+}
