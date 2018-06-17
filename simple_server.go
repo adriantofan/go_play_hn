fdffdf98bf7331d9e4aa12935188ef5b8be16e26
a <a>
2018-06-18 01:35:15 +0200

    topNHandler wip

--- simple_server.go
+++ simple_server.go
@@ -36,14 +36,51 @@ func queryCountHandler(w http.ResponseWriter, r *http.Request, database []record
 	w.Write(result)
 }
 
+// GET /1/queries/popular/<DATE_PREFIX>?size=<SIZE>: returns a JSON object listing the top <SIZE> popular queries that have been done during a specific time range
+// Top 3 popular queries done in 2015: GET /1/queries/popular/2015?size=3: returns
+
+//     {
+//       queries: [
+//         { query: "http%3A%2F%2Fwww.getsidekick.com%2Fblog%2Fbody-language-advice", count: 6675 },
+//         { query: "http%3A%2F%2Fwebboard.yenta4.com%2Ftopic%2F568045", count: 4652 },
+//         { query: "http%3A%2F%2Fwebboard.yenta4.com%2Ftopic%2F379035%3Fsort%3D1", count: 3100 }
+//       ]
+//     }
+// Top 5 popular queries done on Aug 2nd: GET /1/queries/popular/2015-08-02?size=5: returns
+//     {
+//       queries: [
+//         { query: "http%3A%2F%2Fwww.getsidekick.com%2Fblog%2Fbody-language-advice", count: 2283 },
+//         { query: "http%3A%2F%2Fwebboard.yenta4.com%2Ftopic%2F568045", count: 1943 },
+//         { query: "http%3A%2F%2Fwebboard.yenta4.com%2Ftopic%2F379035%3Fsort%3D1", count: 1358 },
+//         { query: "http%3A%2F%2Fjamonkey.com%2F50-organizing-ideas-for-every-room-in-your-house%2F", count: 890 },
+//         { query: "http%3A%2F%2Fsharingis.cool%2F1000-musicians-played-foo-fighters-learn-to-fly-and-it-was-epic", count: 701 }
+//       ]
+//     }
+
+func topNHandler(w http.ResponseWriter, r *http.Request, database []record, urlPrefix string) {
+	w.Header().Add("content-type", "application/json")
+	urlCount := getDistinctQueries(database, urlPrefix, r.URL.Path)
+	result, _ := json.Marshal(struct {
+		Queries []struct{ Query string }
+	}{
+		{{""}},
+	})
+	w.Write(result)
+}
+
 func main() {
 	const queryCountURL string = "/1/queries/count/"
+	const topNURL string = "/1/queries/count/"
+
 	database, errorCount, lineCount := readData("hn_logs.tsv")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		statusHandler(w, r, database, lineCount, errorCount)
 	})
 	http.HandleFunc(queryCountURL, func(w http.ResponseWriter, r *http.Request) {
 		queryCountHandler(w, r, database, queryCountURL)
+	})
+	http.HandleFunc(queryCountURL, func(w http.ResponseWriter, r *http.Request) {
+		topNHandler(w, r, database, topNURL)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
